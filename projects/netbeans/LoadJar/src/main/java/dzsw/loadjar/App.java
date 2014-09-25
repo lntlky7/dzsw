c86301af016a59dd52ec24db718b75f3e71ed6e5
lntlky <cn.soul.sun@gmail.com>
2014-09-25 10:52:24 +0800

    Load Jar

---
+++ projects/netbeans/LoadJar/src/main/java/dzsw/loadjar/App.java
@@ -0,0 +1,13 @@
+package dzsw.loadjar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
